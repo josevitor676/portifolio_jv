f58edd3944820d777b184e40e8f311e0072dc512
josevitor676 <jvss1036@gmail.com>
2022-05-27 12:02:31 -0300

    component header made

---
+++ src/style.js
@@ -0,0 +1,9 @@
+
+import styled from "styled-components"
+
+export const Container = styled.div`
+    background-image: url("https://cdn.pixabay.com/photo/2018/10/16/07/07/stars-3750824_960_720.png");
+    background-repeat: no-repeat;
+    height: auto;
+    background-size: cover;
+`
