7b950cb63b62fd0df9d1f7a0b72c4e831bcfbf8c
josevitor676 <jvss1036@gmail.com>
2022-05-27 13:55:53 -0300

    Components Main Made

---
+++ src/components/main/index.jsx
@@ -0,0 +1,15 @@
+import { MainContainer } from "./style"
+import Button from "../Button"
+
+const Main = () => {
+    return(
+        <MainContainer>
+            <div>
+                <h2>Eu sou um Deselvolvedor Front-End formado na Kenzie Academy Brasil</h2>
+                <Button>Ver Curriculo</Button>
+                <img src="https://www.chihabjraoui.com/media/2021/01/4457.png"/>
+            </div>
+        </MainContainer>
+    )
+}
+export default Main
---
+++ src/components/main/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+export const MainContainer = styled.main`
+    color: white;
+    div {
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        h2 {
+            text-align: center;
+            font-size: 18px;
+            line-height: 30px;
+        }
+        img {
+            width: 270px;
+            margin: 10px auto;
+        }
+    }
+`
